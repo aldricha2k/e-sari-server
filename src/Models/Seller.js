776ee9fc559146777170ddad76bfdcc9708cc583
aldricha2k <159392428+aldricha2k@users.noreply.github.com>
2024-04-12 15:31:32 +0800

    Add files via upload

--- src/Models/Seller.js
+++ src/Models/Seller.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const SellerSchema = new mongoose.model({
+const SellerSchema = new mongoose.Schema({
     name: {
         type: String
     },
